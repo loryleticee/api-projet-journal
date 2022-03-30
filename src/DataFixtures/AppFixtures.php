0c558305b082f9c219827cb698ee7b9807f53c3e
loryleticee <loryleticee@gmail.com>
2022-03-30 15:27:32 +0200

    add trait

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\User;
+use DateTime;
 use Faker\Factory as Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -21,7 +22,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        $user = (new User())->setEmail("lory@lory.fr")->setUsername("lory@lory.fr");
+        $user = (new User())->setEmail("lory@lory.fr")->setUsername("lory@lory.fr")->setCreatedAt(new DateTime())->setUpdateAt(new DateTime());
         $hashedPassword = $this->encoder->hashPassword($user, "lory");
         $user->setPassword($hashedPassword);
         $manager->persist($user);
@@ -30,7 +31,7 @@ class AppFixtures extends Fixture
         Velit laboriosam id, commodi a accusantium earum quod praesentium ullam veritatis ut repellat repellendus expedita voluptatum laudantium perferendis vitae. Ipsum consequatur deserunt, dolore ratione atque eveniet pariatur doloribus eius sunt."];
 
         for ($i = 0; $i < 2; $i++) {
-            $article = (new Article())->setTitle("Article n°$i")->setContent($contents[$i])->setAuthor($user);
+            $article = (new Article())->setTitle("Article n°$i")->setContent($contents[$i])->setAuthor($user)->setCreatedAt(new DateTime())->setUpdateAt(new DateTime());
             $manager->persist($article);
         }
 
@@ -40,7 +41,7 @@ class AppFixtures extends Fixture
             $email = $faker->email;
             $user = (new User())->setEmail($email)->setUsername($email);
             $hashedPassword = $this->encoder->hashPassword($user, $faker->password);
-            $user->setPassword($hashedPassword);
+            $user->setPassword($hashedPassword)->setCreatedAt(new DateTime())->setUpdateAt(new DateTime());
             $manager->persist($user);
         }
 
